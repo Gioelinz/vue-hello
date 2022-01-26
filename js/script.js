6845fadc87ce841a0de12d970796f0c3d19dd823
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-26 13:21:24 +0100

    add hello vue.js & bonus

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 console.log('vue ok', Vue);
 
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
 Vue.config.devtools = true;
 
 const app = new Vue({
     el: '#root',
+    data: {
+        message: 'Hello Vue.Js!',
+        media: 'https://www.facciabuco.com/grafica/vignette/preview_big/toy-story-ovunque.jpg',
+    }
 });
